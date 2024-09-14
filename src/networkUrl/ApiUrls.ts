0cbed844e97249bd1cb2e0432186dbdb84d4be73
siddhant124 <siddhantgoyal3364@gmail.com>
2024-09-14 12:05:25 +0530

    removed API key file

--- src/networkUrl/ApiUrls.ts
+++ src/networkUrl/ApiUrls.ts
@@ -1,4 +1,4 @@
-const GEOAPIFY_SEARCH_PLACES_API_KEY = '29bfe51b75054fcb82d2e6ea5ba61f93';
+import {GEOAPIFY_SEARCH_PLACES_API_KEY} from './SearchPlacesAPIKey';
 
 export const getGeoapifyAutocompleteUrl = (searchText: string): string => {
   return `https://api.geoapify.com/v1/geocode/autocomplete?text=${searchText}&apiKey=${GEOAPIFY_SEARCH_PLACES_API_KEY}`;
